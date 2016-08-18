b07a49bada68c904a8980204a54f3eb530a5c116
Clément Bauchet <clem.ssbm@hotmail.fr>
2016-08-18 13:08:24 +0200

    menu !!!

--- js/states/gameTitle.js
+++ js/states/gameTitle.js
@@ -13,15 +13,15 @@ gameTitle.prototype = {
       var easy_button = game.add.button(game.world.centerX, 400, 'menu_buttons', this.easy, this, 0, 0, 0, 0);
       easy_button.anchor.setTo(0.5);
 
-      var normal_button = game.add.button(game.world.centerX, 450, 'menu_buttons', this.normal, this, 4, 4, 4, 4);
+      var normal_button = game.add.button(game.world.centerX, 450, 'menu_buttons', this.normal, this, 1,1,1,1);
       normal_button.anchor.setTo(0.5);
 
-      var hard_button = game.add.button(game.world.centerX, 500, 'menu_buttons', this.hard, this, 8, 8, 8, 8);
+      var hard_button = game.add.button(game.world.centerX, 500, 'menu_buttons', this.hard, this, 2,2,2,2);
       hard_button.anchor.setTo(0.5);
 
-      var ohgod_button = game.add.button(game.world.centerX, 550, 'menu_buttons', this.ohgod, this, 12, 12, 12, 12);
+      var ohgod_button = game.add.button(game.world.centerX, 550, 'menu_buttons', this.ohgod, this, 3,3,3,3);
       ohgod_button.anchor.setTo(0.5);
-      ohgod_button.animations.add('tilt', [12, 13, 14, 15], 25, true);
+      ohgod_button.animations.add('tilt', [3,4,5,6], 25, true);
       ohgod_button.animations.play('tilt');
    },
 
