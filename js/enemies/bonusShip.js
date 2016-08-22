1ac9eecd26392a77923e60d561530794349f9db8
Clément Bauchet <clem.ssbm@hotmail.fr>
2016-08-22 15:19:04 +0200

    More text

--- js/enemies/bonusShip.js
+++ js/enemies/bonusShip.js
@@ -38,36 +38,6 @@ BonusShip.prototype.dropItem = function(obj) {
       obj.state.killenemy_sd.play();
    }
 
-   //randomly create a bonus
-   // var random = Math.random() * 110;
-   // if (random <= 10) {
-   //    new Powerup(obj.state, x, y, 'powerups', 12, PowerupColl.extraLife);
-   // }
-   // if (random > 10 && random <= 25) {
-   //    new Powerup(obj.state, x, y, 'powerups', 36, PowerupColl.power, true, [36, 37, 38, 39], 18, true);
-   // }
-   // if (random > 25 && random <= 40) {
-   //    new Powerup(obj.state, x, y, 'powerups', 4, PowerupColl.cooldown, true, [4, 5, 6, 7], 18, true);
-   // }
-   // if (random > 40 && random <= 55) {
-   //    new Powerup(obj.state, x, y, 'powerups', 24, PowerupColl.special, true, [24,25,26,27], 18, true);
-   // }           
-   // if (random > 55 && random <= 70) {
-   //    new Powerup(obj.state, x, y, 'powerups', 8, PowerupColl.freeze, true, [8,9,10,11], 18, true);
-   // }
-   // if (random > 70 && random <= 80) {
-   //    new Powerup(obj.state, x, y, 'powerups', 28, PowerupColl.warp, true, [28,29,30,31], 18, true);
-   // }           
-   // if (random > 80 && random <= 90) {
-   //    new Powerup(obj.state, x, y, 'powerups', 32, PowerupColl.kill, true, [32,33,34,35], 18, true);
-   // }           
-   // if (random > 90 && random <= 100) {
-   //    new Powerup(obj.state, x, y, 'powerups', 16, PowerupColl.orange, true, [16,17,18,19], 18, true);
-   // }
-   // if (random > 100 && random <= 110) {
-   //    new Powerup(obj.state, x, y, 'powerups', 40, PowerupColl.bonusLevel, true, [40,41,42,43,44,45,46,47,48,49,50,51,52,53], 18, true);
-   // }
-
    var roulette = ["power","power","power","power",
                    "cooldown","cooldown","cooldown","cooldown",
                    "special","special","special","special",
--- js/states/gameTitle.js
+++ js/states/gameTitle.js
@@ -14,8 +14,7 @@ gameTitle.prototype = {
 
       this.background = game.add.tileSprite(0, 0, game.width, game.height, 'space');
 
-      var text_title = self.game.add.text(self.game.world.width/2, 200, 'SPACE\nINVADERS', {font: '64px Minecraftia', fill: '#4fccff'});
-      text_title.align = 'center';
+      var text_title = self.game.add.text(self.game.world.width/2, 200, 'SPACE\nINVADERS', {font: '64px Minecraftia', fill: '#4fccff', align: 'center'});
       text_title.smoothed = false;
       text_title.fixedToCamera = true;
       text_title.anchor.setTo(0.5);
@@ -23,7 +22,7 @@ gameTitle.prototype = {
       var tween_pulsate = game.add.tween(text_title.scale).to( { x: 1.15, y: 1.15}, 1000, Phaser.Easing.Linear.In, true, 0 , -1);
       tween_pulsate.yoyo(true, 0);
 
-      var text_pauvre = self.game.add.text(500, 260, '(du pauvre)', {font: '12px Minecraftia', fill: '#7fccff'});
+      var text_pauvre = self.game.add.text(500, 275, '(du pauvre)', {font: '12px Minecraftia', fill: '#7fccff'});
       text_pauvre.angle = -30;
       text_pauvre.fixedToCamera = true;
       text_pauvre.smoothed = false;
@@ -36,14 +35,23 @@ gameTitle.prototype = {
       });
       self.timer.start();
 
+      var text_info = self.game.add.text(360, 383, '', {font: '14px Minecraftia', fill: '#00aaff'});
+      text_info.smoothed = false;
+
       var easy_button = game.add.button(game.world.centerX, 400, 'menu_buttons', this.easy, this, 7, 0, 14, 0);
       easy_button.anchor.setTo(0.5);
+      easy_button.events.onInputOver.add(function() {text_info.y = 383; text_info.text = "Pour les joueurs moins\nhabitués aux shoot 'em up.\nPuissance 2 dès le début.\nVitesse un peu plus basse."}, this);
+      easy_button.events.onInputOut.add(function() {text_info.text = ""}, this);
 
       var normal_button = game.add.button(game.world.centerX, 450, 'menu_buttons', this.normal, this, 8,1,15,1);
       normal_button.anchor.setTo(0.5);
+      normal_button.events.onInputOver.add(function() {text_info.y = 433; text_info.text = "La difficulté par défaut.\nVitesse et fréquence des\ntirs ennemis normales."}, this);
+      normal_button.events.onInputOut.add(function() {text_info.text = ""}, this);
 
       var hard_button = game.add.button(game.world.centerX, 500, 'menu_buttons', this.hard, this, 9,2,16,2);
       hard_button.anchor.setTo(0.5);
+      hard_button.events.onInputOver.add(function() {text_info.y = 483; text_info.text = "Vitesse et fréquence des\ntirs ennemis plus élevées.\nMoins de bonus."}, this);
+      hard_button.events.onInputOut.add(function() {text_info.text = ""}, this);
 
       var ohgod_button = game.add.button(game.world.centerX, 550, 'menu_buttons', this.ohgod, this, 10,3,17,3);
       ohgod_button.anchor.setTo(0.5);
@@ -52,9 +60,9 @@ gameTitle.prototype = {
       ohgod_button.animations.add('down', [17,18,19,20], 25, true);
       ohgod_button.animations.play('up');
 
-      ohgod_button.events.onInputOver.add(function() {ohgod_button.animations.play('over')}, this);
-      ohgod_button.events.onInputOut.add(function() {ohgod_button.animations.play('up')}, this);
-      ohgod_button.events.onInputDown.add(function() {ohgod_button.animations.play('down')}, this);
+      ohgod_button.events.onInputOver.add(function() {ohgod_button.animations.play('over'); text_info.y = 533; text_info.fill = '#ff0000'; text_info.text = "MER IL ET FOU §§§"}, this);
+      ohgod_button.events.onInputOut.add(function() {ohgod_button.animations.play('up'); text_info.fill = '#00aaff'; text_info.text = ""}, this);
+      ohgod_button.events.onInputDown.add(function() {ohgod_button.animations.play('down'); text_info.text = "OH GOD"}, this);
 
       var text_difficulty = self.game.add.text(self.game.world.width/2, game.world.height/2 + 40, 'Choisissez la difficulté :', {font: '16px Minecraftia', fill: '#00aaff'});
       text_difficulty.fixedToCamera = true;
